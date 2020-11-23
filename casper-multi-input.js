4e531d371610ad341daac5b2bb4f2194c647b211
AndreSilva1993 <andreemanuel.1993@gmail.com>
2020-11-23 15:13:34 +0000

    Initial commit

---
+++ casper-multi-input.js
@@ -0,0 +1,16 @@
+import '@polymer/paper-input/paper-input.js';
+import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
+
+class CasperMultiInput extends PolymerElement {
+
+  static get template () {
+    return html``;
+  }
+
+  static get properties () {
+    return {
+    };
+  }
+}
+
+window.customElements.define('casper-multi-input', CasperMultiInput);
