a0538c0d78acf80dd4cd3ddc1fbc5a8ff6f2c080
AndreSilva1993 <andreemanuel.1993@gmail.com>
2020-11-26 17:10:26 +0000

    Continuous improvements on the component

--- casper-multi-input.js
+++ casper-multi-input.js
@@ -4,28 +4,56 @@ import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
 
 class CasperMultiInput extends PolymerElement {
 
-  static get SPACE_KEY_CODE () { return 32; }
-  static get SPACE_CHAR_CODE () { return 32; }
-  static get COMMA_KEY_CODE () { return 188; }
-  static get COMMA_CHAR_CODE () { return 44; }
   static get BACKSPACE_KEY_CODE () { return 8; }
 
   static get properties () {
     return {
+      /**
+       * This property defines the type of the casper-multi-input which should have a specific regular expression
+       * to check each value.
+       *
+       * @type {String}
+       */
       type: String,
+      /**
+       * The list of valid values.
+       *
+       * @type {Array}
+       */
       values: {
         type: Array,
-        value: () => ([])
+        value: () => ([]),
+        observer: '__valuesChanged'
       },
+      /**
+       * An array containing all the values, including the invalid ones.
+       *
+       * @type {Array}
+       */
       __internalValues: {
         type: Array,
-        value: () => ([])
+        value: () => ([]),
+        observer: '__internalValuesChanged'
       },
+      /**
+       * An object containing the regular expressions per type of input.
+       *
+       * @type {Object}
+       */
       __regularExpressionsPerType: {
         type: Object,
         value: () => ({
           email: /^\S+@\S+$/
         })
+      },
+      /**
+       * The list of characters that should be used to separate values.
+       *
+       * @type {String}
+       */
+      __separatorCharacters: {
+        type: String,
+        value: ', '
       }
     };
   }
@@ -52,7 +80,6 @@ class CasperMultiInput extends PolymerElement {
       <paper-input
         id="input"
         value="{{__inputValue}}"
-        on-keyup="__onKeyUp"
         on-keydown="__onKeyDown">
         <div id="values-container" slot="prefix">
           <template is="dom-repeat" items="[[__internalValues]]">
@@ -68,11 +95,6 @@ class CasperMultiInput extends PolymerElement {
         </div>
       </paper-input>
     `;
-  }
-
-  ready () {
-    super.ready();
-    window.test = this;
   }
 
   __removeOrEditValue (event) {
@@ -96,22 +118,9 @@ class CasperMultiInput extends PolymerElement {
   }
 
   __onKeyDown (event) {
-    switch (event.keyCode) {
-      case CasperMultiInput.BACKSPACE_KEY_CODE:
-        return this.__backspaceKeyDown();
-      case CasperMultiInput.COMMA_KEY_CODE:
-      case CasperMultiInput.SPACE_KEY_CODE:
-        return this.__spaceOrCommaKeyDown(event);
-    }
-  }
-
-  __onKeyUp (event) {
-    if ([
-      CasperMultiInput.COMMA_KEY_CODE,
-      CasperMultiInput.SPACE_KEY_CODE
-    ].includes(event.keyCode)) {
-      this.__spaceOrCommaKeyUp();
-    }
+    event.keyCode === CasperMultiInput.BACKSPACE_KEY_CODE
+      ? this.__backspaceKeyDown()
+      : this.__remainingKeysDown(event);
   }
 
   __backspaceKeyDown () {
@@ -122,33 +131,61 @@ class CasperMultiInput extends PolymerElement {
     this.__internalValues = [...this.__internalValues];
   }
 
-  __spaceOrCommaKeyDown (event) {
+  __remainingKeysDown (event) {
+    // Completely ignore this method if we're not dealing with one of the separator characters.
+    if (!Array.from(this.__separatorCharacters).includes(event.key)) return;
+
     // Prevent the space and the comma if the input doesn't have any value.
-    if (!this.__inputValue) event.preventDefault();
+    if (!this.__inputValue) return event.preventDefault();
+
+    this.__pushValue(this.__inputValue);
+    setTimeout(() => this.__inputValue = '', 0);
   }
 
-  __spaceOrCommaKeyUp () {
-    // Check if the last character of the input value is either a comma or a space.
-    if ([
-      CasperMultiInput.COMMA_CHAR_CODE,
-      CasperMultiInput.SPACE_CHAR_CODE
-    ].includes(this.__inputValue.charCodeAt(this.__inputValue.length - 1))) {
-      this.__pushValue(this.__inputValue.substring(0, this.__inputValue.length - 1));
-      this.__inputValue = '';
-    }
+  __createValue (value) {
+    const regularExpression = this.__regularExpressionsPerType[this.type] || false;
+
+    return {
+      value,
+      invalid: regularExpression && !regularExpression.test(value)
+    };
   }
 
   __pushValue (value) {
-    this.__internalValues = [
-      ...this.__internalValues,
-      { value, invalid: !this.__validateValue(value) }
-    ];
+    this.__internalValues = [...this.__internalValues, this.__createValue(value)];
   }
+
 
   __validateValue (value) {
     const regularExpression = this.__regularExpressionsPerType[this.type];
 
     return !regularExpression ? true : regularExpression.test(value);
+  }
+
+  /**
+   * This observer is invoked when the values property changes.
+   */
+  __valuesChanged () {
+    // This is used to avoid observer loops since the values and __internalValues properties alter each other.
+    if (this.__valuesLock) return;
+
+    this.__internalValuesLock = true;
+    this.__internalValues = this.values.map(value => this.__createValue(value));
+    this.__internalValuesLock = false;
+  }
+
+  /**
+   * This observer is invoked when the __internalValues property changes.
+   */
+  __internalValuesChanged () {
+    // This is used to avoid observer loops since the values and __internalValues properties alter each other.
+    if (this.__internalValuesLock) return;
+
+    this.__valuesLock = true;
+    this.values = this.__internalValues
+      .filter(internalValue => !internalValue.invalid)
+      .map(internalValue => internalValue.value);
+    this.__valuesLock = false;
   }
 }
 
